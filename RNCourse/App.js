309a6461cb30511b12812703ca2809a0938d922c
Yael <yaelsilvestre64@gmail.com>
2026-06-08 16:18:49 -0600

    Agregacion del botton cancelar

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -12,10 +12,17 @@ export default function App() {
   function startAddGoalHandler(){
     setModalIsVisible(true);
   }
+
+  function endAddGoalHandler(){
+
+    setModalIsVisible(false);
+  }
+
   function addGoalHandler(enteredGoalText){
     setCourseGoals(currentCourseGoals => [
       ...currentCourseGoals,
        {text: enteredGoalText, id: Math.random().toString()}]);
+       endAddGoalHandler();
   };
 
   function deleteGoalHandler(id){
@@ -27,7 +34,11 @@ export default function App() {
   return (
     <View style={styles.appContainer}>
       <Button title='Add New Goal' color="#5e0acc" onPress={startAddGoalHandler}></Button>
-      <GoalInput visible={modalIsVisible} onAddGoal={addGoalHandler} />
+      <GoalInput 
+      visible={modalIsVisible} 
+      onAddGoal={addGoalHandler} 
+      onCancel={endAddGoalHandler} />
+      
       <View style={styles.goalsContainer}>
       <FlatList
       data={courseGoals}
